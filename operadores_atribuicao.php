c6ecb672fbb5d7b49289905ffb9b5b987a1bb288
jhonatanbraga94 <jhonatanbraga1994@gmail.com>
2021-04-14 21:31:47 -0300

    Concatenação e subtração

--- operadores_atribuicao.php
+++ operadores_atribuicao.php
@@ -13,11 +13,20 @@
 
     //http://localhost/estudophp/operadores_atribuicao.php?p=75
 
+    // Exemplo de adição
         $preco = $_GET["p"];
-        echo "O preço do produto é R$ $preco";
+        echo "O preço do produto é R$" .number_format($preco, 2);
         $preco += ($preco*10/100);
-        echo "<br/> e o novo preço com 10% de aumento sera R$ $preco";
+        echo "<br/> E o novo preço com 10% de aumento sera R$" . number_format( $preco, 2);
 
+    // . number_format (variável + 2). Essa expressão é para concatenação, o valor ao lado da variável, informa quantas casas decimais vem após o valor recebido    
+
+    // Exemplo de subtração
+
+    $preco = $_GET["p"];
+    echo "<br/> O preço do produto é R$" . number_format($preco, 2);
+    $preco -= ($preco*10/100);
+    echo "<br/> O preço do produto com o desconto de 10%, fica em R$" . number_format($preco, 2);
 
     ?>
 </body>
