6ebb9e4b1e5373b8cc72d86364b206c10aba2fc8
jhonatanbraga94 <jhonatanbraga1994@gmail.com>
2021-04-15 21:25:54 -0300

    Create operadores_relacionais.php

---
+++ operadores_relacionais.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Opeadores Relacionais</title>
+</head>
+<body>
+<?php
+
+    $n1 = $_GET["a"];
+    $n2 = $_GET["b"];
+    $tipo = $_GET["op"];
+    echo "Os valores passador foram $n1 e $n2 </br>";
+
+
+?>
+</body>
+</html>
