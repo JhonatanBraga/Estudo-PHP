4552a5223bc7cedd5cb834aa8f129ebb929c8bdb
jhonatanbraga94 <jhonatanbraga1994@gmail.com>
2021-04-15 22:05:16 -0300

    Update operadores_relacionais.php

--- operadores_relacionais.php
+++ operadores_relacionais.php
@@ -9,10 +9,42 @@
 <body>
 <?php
 
+# Operadores lógicos em PHP
+# E = and / &&
+# OU = OR / ||
+# OU Exclusivo = XOR
+# Não = !
+
+/*
+# Comparação de valores se é Soma OU Subtração    
     $n1 = $_GET["a"];
     $n2 = $_GET["b"];
     $tipo = $_GET["op"];
-    echo "Os valores passador foram $n1 e $n2 </br>";
+    echo "Os valores passados foram $n1 e $n2 </br>";
+
+    $r = ($tipo == "s")?$n1+$n2 : $n1*$n2;
+    echo "O resultado será $r";
+*/
+
+/*
+# Cálculo de média
+
+$nota1 = $_GET["n1"];
+$nota2 = $_GET["n2"];
+$m = ($nota1 + $nota2) /2;
+echo "A média entre $nota1 e $nota2 é: $m </br>";
+echo "A situação do aluno e" . (($m<6)?"Reprovado":"Aprovado");
+*/
+
+# Verificação entre valores. Ex: Eleição
+
+$ano = $_GET ["an"];
+$idade = 2021 - $ano;
+
+echo "Quem nasceu em $ano tem idade de $idade anos.";
+$tipo = ($idade>= 18 && $idade <=65)?"Obrigatório votar":"Não obrigatório votar";
+echo "</br>Dessa forma você é $tipo";
+
 
 
 ?>
